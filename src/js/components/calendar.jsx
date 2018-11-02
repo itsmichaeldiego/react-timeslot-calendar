871787c3d0001bb63cb280265215f4152d22409b
Michael <itsmichaeldiego@gmail.com>
2018-11-02 21:18:03 +0200

    Use our own version of CalendarJS

--- src/js/components/calendar.jsx
+++ src/js/components/calendar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import CalendarJS from 'calendarjs';
 import Month from './month.jsx';
+import CalendarJS from '../util/calendarjs';
 
 export default class Calendar extends React.Component {
   constructor(props) {
